b1f48fbb94b9f9656477a2c9084ef7884035b88b
zhy1378 <zhy1378@gmail.com>
2015-12-09 01:19:42 +0100

    写了个例子

--- okshell.sh
+++ okshell.sh
@@ -1,10 +1,16 @@
 #!/usr/bin/env bash
 
-# okshell(){
+function function_exists(){
+	FUNCTION_NAME=$1
+	[ -z "$FUNCTION_NAME" ] && return 1
+	declare -F "$FUNCTION_NAME" > /dev/null 2>&1
+	return $?
+}
+
 while [ $# -ne 0 ];do
-	opts=$(echo $1 | tr '=' ' ')
-	opt=${opts[0]}
-	eval on_${opt} ${opts[1]}
+	opts=($(echo $1 | tr '=' ' '))
+	if declare -f "on_${opts[0]}" &>/dev/null;then
+		eval on_${opts[0]} ${opts[1]}
+	fi
 	shift
 done
-# }
--- sample1.sh
+++ sample1.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env bash
+# Run this file:
+# sample1.sh a=1 b=2 c
+
+on_a(){
+	echo a, $1
+}
+
+on_b(){
+	echo b, $1
+}
+
+on_c(){
+	echo $@
+}
 
 source okshell.sh
-
-on_a() {
-	echo a
-	echo $1
-}
-
-on_b() {
-	echo b
-	echo $1
-}
